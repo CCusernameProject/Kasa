496da7bb5932acb9a14b40d38d40d817f9f591d8
Corentin Charmeux <charmeux.corentin.pro@gmail.com>
2024-04-10 06:45:59 +0200

    Fix: Use function .map to get Accommodations

--- src/Router.jsx
+++ src/Router.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
         element: <About />
     },
     {
-        path: `/logement`,
+        path: `/logement/:id`,
         element: <Accommodation />,
     }
 ])
--- src/components/Main/HomeMain/LogementsDisplay.jsx
+++ src/components/Main/HomeMain/LogementsDisplay.jsx
@@ -2,21 +2,19 @@ import Logements from '../../../../logements.json'
 import { Link } from "react-router-dom";
 
 const LogementsDiplay = () => {
-    const images = []
-
-    Logements.forEach((item, index) => {
-        images.push(
-            <div key={index} className='ImageElement'>
-                <img className='ImageElement--image' id={index} src={item.cover} alt={item.title}></img>
-                <div className='BoxShadow'>
-                    <p className='BoxShadow--text'>{item.title}</p>
+    return (
+        <div className='containerBox'>
+            {Logements.map((Logement, index) => (
+                <div key={index} className='ImageElement'>
+                    <img className='ImageElement--image' src={Logement.cover} alt={Logement.title}></img>
+                    <div className='BoxShadow'>
+                        <p className='BoxShadow--text'>{Logement.title}</p>
+                    </div>
+                    <Link className='ImageElement--link' to={`/logement/${Logement.id}`}></Link>
                 </div>
-                <Link className='ImageElement--link' to='/logement'></Link>
-            </div>
-        )
-    })
-
-    return <div className='containerBox'>{images}</div>
+            ))}
+        </div>
+    )
 }
 
 export default LogementsDiplay
